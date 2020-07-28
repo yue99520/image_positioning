7255308fe1821fea1e2ee862950740f2f0f54c0e
林柏儀 <xup61i6u6@gmail.com>
2020-07-28 11:26:11 +0800

    2020/7/28 11:25 ArmControl CM轉換機械手臂視角

--- Library/ArmControl.py
+++ Library/ArmControl.py
@@ -1,15 +1,23 @@
 from pydobot import Dobot
 
 from Library import Coordinate
+from serial.tools import list_ports
+from pydobot import *
+#
+port = list_ports.comports()[0].device
 
 """
 轉換座標
 return 手臂座標{x, y, z}
 """
+finx,finy,finz = 0
 
 
 def convert_arm_coordinate(coord: Coordinate, x, y, z):
-    pass
+    global finx,finy,finz
+    finx = 300-(y*10)
+    finy = (x*10)-180
+    finz = (z-10)-60
 
 
 """
@@ -18,4 +26,9 @@ def convert_arm_coordinate(coord: Coordinate, x, y, z):
 
 
 def move(from_x, from_y, from_z, to_x, to_y, to_z):
-    pass
+    convert_arm_coordinate(from_x,from_y,from_z)
+    bot = Dobot(port=port, verbose=True)
+    bot.move_to(finx,finy,finz)
+    convert_arm_coordinate(to_x, to_y, to_z)
+    bot.move_to(finx,finy,finz)
+
